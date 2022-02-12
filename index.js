5e92642e6ce376dca4a9a3614bb0464d88d57068
Baltazar <balti.kpo@gmail.com>
2022-02-12 12:05:03 -0300

    finished showing games properly

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const API_KEY = 'RGAPI-7a212cce-02c4-44a8-8ade-38130fe79a3e'
 async function getSummonerData(summoner) {
     const SUMMONER_URL = `https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=${API_KEY}`
     let responseSummonerData = await fetch(SUMMONER_URL)
-    const SUMMONER_DATA = await responseSummonerData.json()
-    return SUMMONER_DATA
+    const summonerData = await responseSummonerData.json()
+    return summonerData
 }
 
 async function getMatchDataList(summonerPuuid) {
     const MATCH_ID_LIST_URL = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${summonerPuuid}/ids?start=0&count=20&api_key=${API_KEY}`
     let responseMatchIds = await fetch(MATCH_ID_LIST_URL)
-    const MATCH_ID_LIST = await responseMatchIds.json()
+    const matchIdList = await responseMatchIds.json()
     
-    const MATCH_PROMISES_LIST = MATCH_ID_LIST.map(async id => {
+    const matchPromisesList = matchIdList.map(async id => {
         const MATCH_DATA_URL = `https://americas.api.riotgames.com/lol/match/v5/matches/${id}?api_key=${API_KEY}`
         let responseMatch = await fetch(MATCH_DATA_URL)
         let matchData = await responseMatch.json()
         return matchData
     })
     
-    const MATCH_LIST = await Promise.all(MATCH_PROMISES_LIST)
-    return MATCH_LIST
+    const matchList = await Promise.all(matchPromisesList)
+    return matchList
 }
 
 function matchBox (result) {
@@ -35,7 +35,7 @@ function matchBox (result) {
     }
     return `<div class="${classType}">${InnerText}</div>`
 }
-function winListTest(MatchDataList, playerID) {
+function winList(MatchDataList, playerID) {
     let winListArr = MatchDataList.map((match) => {
         let participants = match.metadata.participants
         let idPosition = participants.indexOf(playerID, 0)
@@ -63,11 +63,7 @@ button.onclick = function () {
         
         getMatchDataList(summonerPuuid).then((MatchDataList) => {
             
-            // const winList = MatchDataList.map((match) => {
-            //     return match.info.teams[0].win
-            // })
-
-            winList = winListTest(MatchDataList, summonerPuuid)
+            winList = winList(MatchDataList, summonerPuuid)
 
             const matchContainerList = winList.map((win) => {
                 return matchBox(win)
@@ -81,3 +77,39 @@ button.onclick = function () {
     
     
 }
+
+// function matchBox (result) {
+//     let classType;
+//     let InnerText;
+//     if (result) {
+//         classType = 'won'
+//         InnerText = 'Victory'
+//     } else {
+//         classType = 'lost'
+//         InnerText = 'Defeat'
+//     }
+//     return `<div class="${classType}">${InnerText}</div>`
+
+// button.onclick = function () {
+//     let summonerName = summonerNameInput.value
+
+//     getSummonerData(summonerName).then((summonerPromise) => {
+//         summonerData = summonerPromise
+//         summonerPuuid = summonerData.puuid
+        
+//         getMatchDataList(summonerPuuid).then((MatchDataList) => {
+            
+//             winList = winList(MatchDataList, summonerPuuid)
+
+//             const matchContainerList = winList.map((win) => {
+//                 return matchBox(win)
+//             })
+//             matchesContainer.innerHTML = `
+//             ${matchContainerList.join("")}
+//             `
+//         })
+    
+//     })
+    
+    
+// }
